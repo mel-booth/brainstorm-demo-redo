99c4839bbbf56eba800ba343323dc9416ffd6417
Mel Booth <mel.m.booth@gmail.com>
2017-01-26 09:51:42 +1300

    add basic style to input

--- components/app.js
+++ components/app.js
@@ -4,10 +4,12 @@ const App = (props) => {
   return (
     <div>
       <h1 id='heading'>Brainstorm Democracy</h1>
+      <div className='idea-input'>
       <input id='idea' type='text' placeholder='Enter your idea' />
       <input className='btn' type='submit' value='Submit' onClick={(e) => {
         props.dispatch({type:'ADD_IDEA', payload: {details: e.target.previousSibling.value, votes: 0}})
       }} />
+      </div>
     </div>
   )
 }

b546dd113110991e381b143b8ecc4367b9e65616
Mel Booth <mel.m.booth@gmail.com>
2017-01-26 11:10:39 +1300

    add hover to li and add vote-text

--- components/idea.js
+++ components/idea.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react'
 const Idea = (props) => {
   return (
     <ul>
-      <li className='idea' onClick={() => props.dispatch({type: 'INCREMENT_VOTE', payload: props.index})}> <span id='votes'>{props.votes} </span>
-      <span id='idea'>{props.details} </span>
+      <li className='idea' onClick={() => props.dispatch({type: 'INCREMENT_VOTE', payload: props.index})}> <span id='votes'>{props.votes}</span><span id='vote-text'>vote/s</span>
+      <span id='idea'>{props.details}</span>
       </li>
     </ul>
   )

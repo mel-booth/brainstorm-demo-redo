4f1fab422511951f4b254bd7d60dbf510f46f153
Mel Booth <mel.m.booth@gmail.com>
2017-01-26 09:30:08 +1300

    list css considerations - add ids/classess where needed

--- components/idea.js
+++ components/idea.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react'
 const Idea = (props) => {
   return (
     <ul>
-      <li onClick={() => props.dispatch({type: 'INCREMENT_VOTE', payload: props.index})}>Idea: {props.details} Votes: {props.votes}</li>
+      <li className='idea' onClick={() => props.dispatch({type: 'INCREMENT_VOTE', payload: props.index})}> {props.details}
+      <span id='votes'>{props.votes} </span></li>
     </ul>
   )
 }

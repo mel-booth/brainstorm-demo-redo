e247f1be05b4818c7dde5a98b1ee10bd91781905
Mel Booth <mel.m.booth@gmail.com>
2017-01-25 16:18:00 +1300

    attempt at writing first test - watch it fail

--- test/test.js
+++ test/test.js
@@ -7,8 +7,35 @@ test('testing Brainstorm-Democracy-V2', function (t) {
   t.end()
 })
 
-test('ADD_IDEA', function (t) {
+test('ADD_IDEA adds an idea to list', function (t) {
+  //arrange
+  const state = {
+    ideas: [
+      { id: 1, details: 'Get a job', votes: 7 },
+      { id: 2, details: 'Work, work, work, work, work', votes: 5 },
+      { id: 3, details: 'Drink more coffee', votes: 3 }
+    ],
+  }
 
+  const expected = {
+    ideas: [
+      { id: 1, details: 'Get a job', votes: 7 },
+      { id: 2, details: 'Work, work, work, work, work', votes: 5 },
+      { id: 3, details: 'Drink more coffee', votes: 3 },
+      { id: 4, details: 'Meditate', votes: 4 }
+    ],
+  }
+
+  const action = {type: 'ADD_IDEA', payload: { id: 4, details: 'Meditate', votes: 4 } }
+
+  freeze(state)
+
+  //act
+  const actual = reducer(state, action)
+
+  //assert
+  t.deepEqual(actual, expected, "adds a bloomin' idea mate!")
+  t.end()
 })
 
 test('INCREMENT_VOTE', function (t) {

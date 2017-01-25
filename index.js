4973d801259f32fd40682f2a113b1b27da7d4abe
Mel Booth <mel.m.booth@gmail.com>
2017-01-25 15:55:59 +1300

    fix reducer typo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 
+import reducer from './reducer'
 import App from './components/app'
 
 const initialState = {
--- reducer.js
+++ reducer.js
@@ -2,15 +2,17 @@ import clone from 'clone'
 
 function reducer (state, action) {
   const newState = clone(state)
+
   switch (action.type) {
     case 'ADD_IDEA':
       return newState
-  }
+
     case 'INCREMENT_VOTE':
       return newState
 
     default:
     return newState
+  }
 
 }
 

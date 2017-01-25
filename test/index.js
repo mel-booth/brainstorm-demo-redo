73b80b6ad66740893b42b6ef0ffb3b88285b4ff9
Mel Booth <mel.m.booth@gmail.com>
2017-01-25 15:41:35 +1300

    list test cases

--- test/index.js
+++
@@ -1,6 +0,0 @@
-var test = require('tape')
-
-test('testing Brainstorm-Democracy-V2', function (t) {
-  t.ok(true)
-  t.end()
-})
---
+++ test/test.js
@@ -0,0 +1,20 @@
+import test from 'tape'
+import freeze from 'deep-freeze'
+
+
+test('testing Brainstorm-Democracy-V2', function (t) {
+  t.ok(true)
+  t.end()
+})
+
+test('ADD_IDEA', function (t) {
+
+})
+
+test('INCREMENT_VOTE', function (t) {
+
+})
+
+test('DECREMENT_VOTE', function (t) {
+
+})

09e6db2562dce8599a6194c68fdc0e4d12d6be0f
Mel Booth <mel.m.booth@gmail.com>
2017-01-26 10:26:08 +1300

    replace input with button

--- components/app.js
+++ components/app.js
@@ -6,9 +6,9 @@ const App = (props) => {
       <h1 id='heading'>Brainstorm Democracy</h1>
       <div className='idea-input'>
       <input id='idea-input' type='text' placeholder='Enter your idea' />
-      <input className='btn' type='submit' value='Submit' onClick={(e) => {
+      <button id='btn' type='submit' value='Submit' onClick={(e) => {
         props.dispatch({type:'ADD_IDEA', payload: {details: e.target.previousSibling.value, votes: 0}})
-      }} />
+      }}>Submit Idea</button>
       </div>
     </div>
   )

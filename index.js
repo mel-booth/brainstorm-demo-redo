688952bbfa33c8fd038e5d1d5901b1c32639e12c
Mel Booth <mel.m.booth@gmail.com>
2017-01-25 15:32:04 +1300

    set up initialState

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+
 import App from './components/app'
+
+const initialState = {
+  ideas: [
+    {
+      id: 1, details: 'Get a job', votes: 7
+    },
+    {
+      id: 2, details: 'Work, work, work, work, work', votes: 5
+    },
+    {
+      id: 3, details: 'Drink more coffee', votes: 3
+    }
+  ]
+}
 
 render(<App name='Brainstorm-Democracy-V2' />, document.querySelector('main'))
 console.log('welcome to Brainstorm-Democracy-V2')
